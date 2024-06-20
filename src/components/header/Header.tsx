9af54efb0d31f6255334dac5809d5aed5e25a8f7
Mshwadfi <mohamedalshwadfy24@gmail.com>
2024-06-20 20:29:25 +0300

    added hamburger menu

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,15 +1,29 @@
+'use client'
+import { useState } from 'react';
 import Link from 'next/link';
 import styles from './header.module.css';
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className={styles.container}>
-     
       <div className={styles.logo}>BlogMe</div>
-      <div className={styles.links}>
-        <Link href="/" className={styles.link}>Homepage</Link>
-        <Link href="/createBlog" className={styles.link}>Create Blog</Link>
+      <div className={styles.hamburger} onClick={toggleMenu}>
+        <div className={styles.hamburgerLine} ></div>
+        <div className={styles.hamburgerLine}></div>
+        <div className={styles.hamburgerLine}></div>
       </div>
+      {isMenuOpen && (
+        <div className={styles.menu}>
+          <Link href="/" className={styles.link} onClick={toggleMenu}>Homepage</Link>
+          <Link href="/createBlog" className={styles.link} onClick={toggleMenu}>Create Blog</Link>
+        </div>
+      )}
     </div>
   );
 };
